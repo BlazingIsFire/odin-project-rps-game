89628da916e1faf6843a97412943bd2c89c6d790
BlazingIsFire <firemanandy1102@gmail.com>
2022-10-05 19:46:51 -0400

    Updated HTML & CSS. Completed Javascript; Game is complete.

--- index.js
+++ index.js
@@ -18,10 +18,18 @@ var paper = options[1];
 var scissors = options[2];
 var playerChoice = "";
 var computerChoice = "";
+let playerScore = 0;
+let computerScore = 0;
+var gameWinner = "";
 var rockbutton = document.querySelector("#rock");
 var paperbutton = document.querySelector("#paper");
 var scissorsbutton = document.querySelector("#scissors");
-var buttons = document.querySelectorAll(".btn");
+var playerScoreLabel = document.querySelector(".score-label-player");
+var computerScoreLabel = document.querySelector(".score-label-computer");
+var footerLabel = document.querySelector(".footer-text-label");
+var endGameModal = document.getElementById("end-game-modal");
+var endGameButton = document.querySelector(".end-game-btn");
+var endGameWinner = document.querySelector(".end-game-winner");
 
 // Button selections
 rockbutton.onclick = function(){
@@ -38,8 +46,8 @@ scissorsbutton.onclick = function(){
 }
 
 function getComputerChoice() {
-    computerChoice = options[Math.floor(Math.random() * options.length)];
-    console.log(computerChoice);
+        computerChoice = options[Math.floor(Math.random() * options.length)];
+        console.log(computerChoice);
 }
 getComputerChoice();
 
@@ -59,10 +67,46 @@ function winner(playerChoice, computerChoice) {
 
 function playRound(playerChoice, computerChoice) {
     if (winner(playerChoice, computerChoice) === "Tie"){
-        console.log("It's a tie.");
+        footerLabel.innerHTML = "It's a Tie!";
     } else if (winner(playerChoice, computerChoice) === "Player"){
-        console.log("You won!");
-    } else {
-        console.log("The computer won :(");
+        playerScore++;
+        playerScoreLabel.innerHTML = playerScore;
+        footerLabel.innerHTML = "You won the round!";
+    } else if (winner(playerChoice, computerChoice) === "Computer") {
+        computerScore++;
+        computerScoreLabel.innerHTML = computerScore;
+        footerLabel.innerHTML = "Yikes... the computer won.";
     }
+    if (playerScore === 5 || computerScore === 5){
+        declareWinner(playerScore, computerScore);
+    } 
+    getComputerChoice();
+}
+
+function declareWinner(playerScore, computerScore) {
+    if (playerScore === 5){
+        gameWinner = "You";
+    } else if (computerScore === 5){
+        gameWinner = "Computer";
+    }
+    endGame(gameWinner);
+}
+
+function endGame(gameWinner) {
+    if (gameWinner === "You"){
+        endGameWinner.innerHTML = "You have won the game!"
+    } else if (gameWinner === "Computer"){
+        endGameWinner.innerHTML = "Computer has won the game!"
+    }
+    endGameModal.style.display = "flex";
+}
+
+endGameButton.onclick = function(){
+    playerScore = 0;
+    computerScore = 0;
+    playerScoreLabel.innerHTML = playerScore;
+    computerScoreLabel.innerHTML = computerScore;
+    footerLabel.innerHTML = "Select a option to play!"
+    endGameModal.style.display = "none";
+    getComputerChoice();
 }
